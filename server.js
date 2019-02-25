47e690b04851155685b17270f7abba3161869df5
Senn Pateet <senn.patteet@hotmail.com>
2019-02-25 13:52:31 +0100

    interface (not done) and display messages

--- server.js
+++ server.js
@@ -7,15 +7,41 @@ const io = require('socket.io').listen(server);
 
 const nodemon = require('nodemon');
 
-users = [];
-connections = []
-
 server.listen(process.env.PORT || 3000);
 console.log('Server running...');
+
+app.set('view engine', 'ejs');
+
+app.use(express.static('public'));
 
 
 // ROUTES -------------------------------------------------------
 
-app.get('/', (req, res) => {
-  res.sendFile(__dirname + '/index.html')
-})
+app.get('/', function(req, res){
+  res.sendFile(__dirname + '/views/index.html');
+});
+
+io.on('connection', function(socket){
+  console.log('a user connected');
+  socket.on('disconnect', function(){
+    console.log('user disconnected');
+  });
+});
+
+io.on('connection', function(socket){
+  socket.on('chat message', function(msg){
+    console.log('message: ' + msg);
+  });
+});
+
+io.emit('some event', { for: 'everyone' });
+
+io.on('connection', function(socket){
+  socket.broadcast.emit('hi');
+});
+
+io.on('connection', function(socket){
+  socket.on('chat message', function(msg){
+    io.emit('chat message', msg);
+  });
+});
